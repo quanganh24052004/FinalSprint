06044998e242db19f2fb6704656fccfc344b1d1b
Anh Nguyen Quang <anh.nguyenquang24052004@gmail.com>
2025-08-26 04:57:29 +0700

    Initial Commit

---
+++ FinalSprint/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinalSprint
+//
+//  Created by EF2025 on 26/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinalSprint/FinalSprintApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalSprintApp.swift
+//  FinalSprint
+//
+//  Created by EF2025 on 26/8/25.
+//
+
+import SwiftUI
+
+@main
+struct FinalSprintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
